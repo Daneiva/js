6eb7d9e5ceabbc25862ae16f7ab0a06e0d8b35e4
daneiva gamboa <daneiva@gmail.com>
2015-03-19 16:39:06 -0400

    exercises practice 1

--- js/controlStructures.js
+++ js/controlStructures.js
@@ -1,6 +1,7 @@
 /**
  * Created by daneiva gamboa on 3/19/2015.
  */
+
 var getFirstCapicua= function(init, limit){
 
     window.alert(getFirstCapicua());
@@ -11,6 +12,28 @@ var getFirstCapicua= function(init, limit){
     if(rev== ser){
 
     }
+    else
+    {
+
     }
-    return    /*first capicua number form the range*/
+    }
+    return   /*first capicua number form the range*/
+
+
+for (var i; i=init;i <= limit; i++){
+    if (!iscapicua(i)){
+        cotinue;
+    }
+    return i;
+};
+
+var getFirstCapicuaSV= function(init, limit){
+    for (var i=init;i <= limit; i++){
+        if (iscapicua(i)){
+        continue;
+        }
+        return(i);
+    };
+
+    return   /*first capicua number form the range*/
 };
