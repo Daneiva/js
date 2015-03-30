be254310ce263ea63dab1fb2cc3ebadccb98d337
Alejandra Arteaga <alejan_dra01@outlook.com>
2015-03-30 15:54:22 -0400

    3/30 commit date

--- GameBattle/js/field.js
+++ GameBattle/js/field.js
@@ -1,13 +1,13 @@
 /**
- * Created by daneiva gamboa on 3/23/2015.
+ * Created by daneiva gamboa -Jimmy Maldonado
  */
 /*
-* field board
-*/
+ * field board
+ */
 var Field= function(siz,numShips) {
     var _size = siz;
     var _numberOfShips=numShips;
-    var _ships = new Array(numShips);
+    var _ships = new Array();/*var _ships = new Array();*/
     /*Create the field (size*size) and fill the board with 0*/
     var _matriz = new Array(_size);
 
@@ -16,7 +16,7 @@ var Field= function(siz,numShips) {
     }
     for (var i = 0; i < _size; i++) {
         for (var j = 0; j < _size; j++) {
-            _matriz[i][j]=0;
+            _matriz[i][j]="0";
         }
     }
 
@@ -27,15 +27,18 @@ var Field= function(siz,numShips) {
             console.log('\n');
         }
     };
+    this.getField=function(){
 
+        return _matriz;
+    };
     /*
-    * Put the Ships on the board
-    * */
+     * Put the Ships on the board
+     * */
     this.putShips=function() {
         var _max = 3;
         var _shipCreated=0;
         while(_shipCreated < _numberOfShips){
-            var _sizeX = Math.floor((Math.random() * _max-min+1) + min);
+            var _sizeX = Math.floor((Math.random() * _max) + 1);
             var _sizeY = _sizeX==1 ?  Math.floor((Math.random() * _max) + 1): 1;
             var _posX = Math.floor((Math.random() * (_size)));
             var _posY = Math.floor((Math.random() * (_size)));
@@ -43,11 +46,11 @@ var Field= function(siz,numShips) {
             var _positionY = _size > (_sizeY + _posY) ? _posY: (_posY - ((_sizeY+_posY)-_size));
             var _busy = verifyPosition(_sizeX,_sizeY,_positionX,_positionY);
             if(!_busy) {
-                _ships.push(new Ship(_sizeX,_sizeY,_positionX,_positionY));
+                _ships.push(new Ship(_sizeX,_sizeY,_positionX,_positionY,_matriz));
                 for (var i = 0; i < _sizeX; i++) {
                     for (var j = 0; j < _sizeY; j++) {
-                        if (_matriz[_positionY + j][_positionX + i] == 0) {
-                            _matriz[_positionY + j][_positionX + i] = 1;
+                        if (_matriz[_positionY + j][_positionX + i] == "0") {
+                            _matriz[_positionY + j][_positionX + i] = "1";
                         }
                     }
                 }
@@ -60,14 +63,21 @@ var Field= function(siz,numShips) {
         }
     };
 
+    this.getShips=function(){
+        return _ships;
+
+    };
+
+    /*ships=game.getShips()*/
+
     /*
-    Verify if is possible to create a ship
+     Verify if is possible to create a ship
      */
     var verifyPosition = function (sizeX,sizeY,positionX,positionY) {
         var _res = false;
         for (var i = 0; i < sizeX; i++) {
             for (var j = 0; j < sizeY; j++) {
-                if (_matriz[positionY+j][positionX+i]==1) {
+                if (_matriz[positionY+j][positionX+i]=="1") {
                     _res=true;
                     break;
                 }
@@ -76,6 +86,4 @@ var Field= function(siz,numShips) {
         return _res;
     };
 };
-var game = new Field(8,3);
-game.putShips();
-game.showTable();
+
