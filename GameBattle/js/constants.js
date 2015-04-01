97cca4df14d86e71cab7b9214854a285ba036499
Jimmy Maldonado <jimmy29011993@gmail.com>
2015-04-01 12:41:38 -0400

    Corrections on "Field" class

--- GameBattle/js/constants.js
+++ GameBattle/js/constants.js
@@ -1,13 +1,17 @@
 
 /* Class Constants, Execute First to inicialize Constants*/
-var CONSTANTS = function(size,ships,sizeShip){
+var CONSTANTS = function(){
     var _constants = {
         //Board Size
-        BOARD_SIZE: size,
+        BOARD_SIZE: 10,
         //Ships to Create
-        TOTAL_SHIPS: ships,
-        // Max Size of Ship
-        MAX_SIZE_SHIP: sizeShip
+        TOTAL_SHIPS: 5,
+        //Max Size of Ship
+        MAX_SIZE_SHIP: 3,
+        //Busy space
+        BUSY_SPACE: '1',
+        //Blank space
+        BLANK_SPACE: '0'
     };
     return {
         get: function(key) {
